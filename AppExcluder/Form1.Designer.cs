1d3374854ca473cc8ce228f8887d0e86da1ca660
Porya <gohary.porya@gmail.com>
2019-09-26 17:36:26 +0330

    First Commit!

---
+++ AppExcluder/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace AppExcluder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            this.label1.Location = new System.Drawing.Point(24, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Default Routing Adapter:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(175, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(254, 23);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(471, 270);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ AppExcluder/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppExcluder
+{
+    public partial class Form1 : Form
+    {
+        private String Selected_Adapter;
+        public System.Windows.Forms.ComboBox.ObjectCollection Items { get; }
+        
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < net_adapters().Count; i++)
+            {
+                comboBox1.Items.Add(net_adapters()[i]);
+            }
+            
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Selected_Adapter = comboBox1.SelectedText;
+            Console. WriteLine(Selected_Adapter);
+            Console. WriteLine("Hello");
+            //throw new System.NotImplementedException();
+        }
+        
+        public List<String> net_adapters() 
+        { 
+            List<String> values = new List<String>();
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces()) 
+            { 
+                values.Add(nic.Name);
+            } 
+            return values; 
+        }
+    }
+    
+    
+}
