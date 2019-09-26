8188d43d58668f3894e7fa45629f3a5932a2ead7
Porya <gohary.porya@gmail.com>
2019-09-26 22:15:13 +0330

    Fix Get IP Address And Run From CMD

--- AppExcluder/Form1.Designer.cs
+++ AppExcluder/Form1.Designer.cs
@@ -31,23 +31,44 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            this.label1.Location = new System.Drawing.Point(24, 19);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(24, 18);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(145, 22);
             this.label1.TabIndex = 0;
             this.label1.Text = "Default Routing Adapter:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(175, 16);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(254, 23);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(276, 113);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "RUN!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(471, 270);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Location = new System.Drawing.Point(15, 15);
@@ -60,5 +81,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- AppExcluder/Form1.cs
+++ AppExcluder/Form1.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,16 +16,20 @@ namespace AppExcluder
     public partial class Form1 : Form
     {
         private String Selected_Adapter;
-        public System.Windows.Forms.ComboBox.ObjectCollection Items { get; }
-        
+        List<String> Adapters = new List<String>();
+
+        //Start CMD Window Hidden
+        Process cmd = new Process();
+
+
         public Form1()
         {
             InitializeComponent();
-            for (int i = 0; i < net_adapters().Count; i++)
+            Adapters = net_adapters();
+            for (int i = 0; i < Adapters.Count; i++)
             {
-                comboBox1.Items.Add(net_adapters()[i]);
+                comboBox1.Items.Add(Adapters[i]);
             }
-            
         }
 
         private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
@@ -38,22 +44,92 @@ namespace AppExcluder
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Selected_Adapter = comboBox1.SelectedText;
-            Console. WriteLine(Selected_Adapter);
-            Console. WriteLine("Hello");
+            Selected_Adapter = comboBox1.SelectedItem.ToString();
+            //Console. WriteLine(Selected_Adapter);
             //throw new System.NotImplementedException();
         }
-        
-        public List<String> net_adapters() 
-        { 
+
+        public List<String> net_adapters()
+        {
             List<String> values = new List<String>();
-            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces()) 
-            { 
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
                 values.Add(nic.Name);
-            } 
-            return values; 
+            }
+
+            return values;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String command = "\"C:\\Program Files\\Mozilla Firefox\\firefox.exe\"";
+
+            try
+            {
+                //Asynchronously start the Thread to process the Execute command request.
+                Thread objThread = new Thread(new ParameterizedThreadStart(ExecuteCommandSync));
+                //Make the thread as background thread.
+                objThread.IsBackground = true;
+                //Set the Priority of the thread.
+                objThread.Priority = ThreadPriority.AboveNormal;
+                //Start the thread.
+                objThread.Start(command);
+            }
+            catch (ThreadStartException objException)
+            {
+                // Log the exception
+            }
+
+            String IP = getIP(Selected_Adapter);
+        }
+
+        public String getIP(String name)
+        {
+            String IP = null;
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                Console.Write(nic.Name+"\n");
+                if (nic.Name.ToString().Equals(name))
+                {
+                    foreach (UnicastIPAddressInformation ip in nic.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            Console.WriteLine(ip.Address.ToString());
+                            return IP;
+                        }
+                    }
+                    
+                    
+                }
+            }
+            
+            return null;
+        }
+
+        public void ExecuteCommandSync(object command)
+        {
+            // create the ProcessStartInfo using "cmd" as the program to be run,
+            // and "/c " as the parameters.
+            // Incidentally, /c tells cmd that we want it to execute the command that follows,
+            // and then exit.
+            System.Diagnostics.ProcessStartInfo procStartInfo =
+                new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+
+            // The following commands are needed to redirect the standard output.
+            // This means that it will be redirected to the Process.StandardOutput StreamReader.
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            // Do not create the black window.
+            procStartInfo.CreateNoWindow = true;
+            // Now we create a process, assign its ProcessStartInfo and start it
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            // Get the output into a string
+            string result = proc.StandardOutput.ReadToEnd();
+            // Display the command output.
+            Console.WriteLine(result);
         }
     }
-    
-    
 }
